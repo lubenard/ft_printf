fc6a096651f8c9c366ef5c8f6171900fd877217c
lubenard <lubenard@student.42.fr>
2019-04-17 23:32:37 +0200

    Norme almost fixed for %di, have to move the functions

--- srcs/flags/percent_di.c
+++ srcs/flags/percent_di.c
@@ -6,7 +6,7 @@
 /*   By: lubenard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/30 20:01:12 by lubenard          #+#    #+#             */
-/*   Updated: 2019/04/17 15:23:39 by lubenard         ###   ########.fr       */
+/*   Updated: 2019/04/17 23:15:47 by lubenard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,34 +28,94 @@ char		*get_option_d(va_list ap, t_word *lkd_list)
 		return (ft_itoa(va_arg(ap, int)));
 }
 
-int			handle_ret_di(t_percent_di struct_di, t_word *lkd_list,
+int			handle_ret_di(t_percent_di *struct_di, t_word *lkd_list,
 		char *to_remplace)
 {
 	free(lkd_list->content);
-	if (struct_di.spaces != NULL)
+	if (struct_di->spaces != NULL)
 	{
-		if (struct_di.prec != NULL)
-			free(struct_di.prec);
-		if (struct_di.tmp != NULL)
-			free(struct_di.tmp);
+		if (struct_di->prec != NULL)
+			free(struct_di->prec);
+		if (struct_di->tmp != NULL)
+			free(struct_di->tmp);
 		free(to_remplace);
-		lkd_list->content = change_minus(struct_di.spaces);
+		lkd_list->content = change_minus(struct_di->spaces);
 	}
-	else if (struct_di.tmp != NULL)
+	else if (struct_di->tmp != NULL)
 	{
-		if (struct_di.prec != NULL)
-			free(struct_di.prec);
+		if (struct_di->prec != NULL)
+			free(struct_di->prec);
 		free(to_remplace);
-		lkd_list->content = change_minus(struct_di.tmp);
+		lkd_list->content = change_minus(struct_di->tmp);
 	}
-	else if (struct_di.prec != NULL)
+	else if (struct_di->prec != NULL)
 	{
 		free(to_remplace);
-		lkd_list->content = change_minus(struct_di.prec);
+		lkd_list->content = change_minus(struct_di->prec);
 	}
 	else
 		lkd_list->content = change_minus(to_remplace);
 	return (0);
+}
+
+int			handle_errors_di(t_word *lkd_list, char **to_remplace)
+{
+if (ft_strcmp(*to_remplace, "0") == 0
+	&& (ft_strstr(lkd_list->content, ".0")
+	|| detect_prec(lkd_list->content, 0) == -1))
+	{
+		if (lkd_list->spaces > 0)
+		{
+			free(*to_remplace);
+			*to_remplace = ft_strdup("");
+		}
+		else
+		{
+			free(lkd_list->content);
+			free(*to_remplace);
+			lkd_list->is_malloc = 0;
+			lkd_list->content = "";
+			return (0);
+		}
+	}
+	return (1);
+}
+
+t_percent_di	initialise_struct_di(void)
+{
+	t_percent_di struct_di;
+
+	struct_di.spaces = NULL;
+	struct_di.prec = NULL;
+	struct_di.tmp = NULL;
+	return (struct_di);
+}
+
+void		handle_plus_di(t_percent_di *struct_di, t_word *lkd_list, char *to_remplace)
+{
+	if (struct_di->prec && ft_strchr(lkd_list->content, '+') != -1
+	&& ft_strchr(struct_di->prec, '-'))
+		struct_di->tmp = ft_strjoin("+", struct_di->prec);
+	else if (struct_di->prec && ft_strchr(lkd_list->content, ' ') != -1
+	&& ft_strchr(struct_di->prec, '-'))
+		struct_di->tmp = ft_strjoin(" ", struct_di->prec);
+	else if (ft_strchr(lkd_list->content, '+') != -1
+	&& ft_strchr(to_remplace, '-') && struct_di->prec == NULL)
+		struct_di->tmp = ft_strjoin("+", to_remplace);
+	else if (ft_strchr(lkd_list->content, ' ') != -1
+	&& ft_strchr(to_remplace, '-') && struct_di->prec == NULL)
+		struct_di->tmp = ft_strjoin(" ", to_remplace);
+}
+
+void		handle_ret_di2(t_percent_di *struct_di, t_word *lkd_list, char *to_remplace, int i)
+{
+	if (extract_prec(lkd_list->content, i) == ft_strlen(to_remplace)
+	&& extract_prec(lkd_list->content, i) != 0 && ft_strchr(lkd_list->content, '+') == -1)
+	{
+		lkd_list->is_neg = 1;
+		free(struct_di->spaces);
+		struct_di->spaces = ft_strjoin("0", struct_di->prec);
+	}
 }
 
 int			percent_di(t_word *lkd_list, va_list ap)
@@ -64,42 +124,13 @@ int			percent_di(t_word *lkd_list, va_list ap)
 	int				i;
 	t_percent_di	struct_di;
 
-	struct_di.spaces = NULL;
-	struct_di.prec = NULL;
-	struct_di.tmp = NULL;
+	struct_di = initialise_struct_di();
 	to_remplace = get_option_d(ap, lkd_list);
-	if (ft_strcmp(to_remplace, "0") == 0
-	&& (ft_strstr(lkd_list->content, ".0")
-	|| detect_prec(lkd_list->content, 0) == -1))
-	{
-		if (lkd_list->spaces > 0)
-		{
-			free(to_remplace);
-			to_remplace = ft_strdup("");
-		}
-		else
-		{
-			free(lkd_list->content);
-			free(to_remplace);
-			lkd_list->is_malloc = 0;
-			lkd_list->content = "";
-			return (0);
-		}
-	}
+	if (handle_errors_di(lkd_list, &to_remplace) == 0)
+		return (0);
 	if ((i = ft_strchr(lkd_list->content, '.')) != -1)
 		struct_di.prec = precision(lkd_list->content, to_remplace, i, 0);
-	if (struct_di.prec && ft_strchr(lkd_list->content, '+') != -1
-	&& ft_strchr(struct_di.prec, '-'))
-		struct_di.tmp = ft_strjoin("+", struct_di.prec);
-	else if (struct_di.prec && ft_strchr(lkd_list->content, ' ') != -1
-	&& ft_strchr(struct_di.prec, '-'))
-		struct_di.tmp = ft_strjoin(" ", struct_di.prec);
-	else if (ft_strchr(lkd_list->content, '+') != -1
-	&& ft_strchr(to_remplace, '-') && struct_di.prec == NULL)
-		struct_di.tmp = ft_strjoin("+", to_remplace);
-	else if (ft_strchr(lkd_list->content, ' ') != -1
-	&& ft_strchr(to_remplace, '-') && struct_di.prec == NULL)
-		struct_di.tmp = ft_strjoin(" ", to_remplace);
+	handle_plus_di(&struct_di, lkd_list, to_remplace);
 	if ((ft_isdigit(lkd_list->content[1]) ||
 	lkd_list->content[1] == '-' || lkd_list->content[1] == '+')
 		&& struct_di.prec == NULL && struct_di.tmp == NULL)
@@ -110,12 +141,6 @@ int			percent_di(t_word *lkd_list, va_list ap)
 	else if ((ft_isdigit(lkd_list->content[1]) || lkd_list->content[1] == '-')
 		&& struct_di.prec != NULL)
 		struct_di.spaces = add_space(lkd_list, struct_di.prec);
-	if (extract_prec(lkd_list->content, i) == ft_strlen(to_remplace)
-	&& extract_prec(lkd_list->content, i) != 0 && ft_strchr(lkd_list->content, '+') == -1)
-	{
-		lkd_list->is_neg = 1;
-		free(struct_di.spaces);
-		struct_di.spaces = ft_strjoin("0", struct_di.prec);
-	}
-	return (handle_ret_di(struct_di, lkd_list, to_remplace));
+	handle_ret_di2(&struct_di, lkd_list, to_remplace, i);
+	return (handle_ret_di(&struct_di, lkd_list, to_remplace));
 }
